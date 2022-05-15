77564566bcaaec303f74621ccb17cbe82c655e80
Oleksii <zankexd@icloud.com>
2022-05-15 17:57:55 +0200

    lesson

---
+++ 3 task/Program.cs
@@ -0,0 +1,25 @@
+﻿int Lock( int a)
+{
+    if(a<100)
+    {
+        Console.WriteLine ("Нет 3  числа");
+    }
+
+    else if( a>100){
+
+    
+    while (a>1000)
+    {
+    int b = 10;
+    a = a / b;
+    
+    b=b* 10; 
+    }
+
+    Console.WriteLine(a%10);
+    }
+    return a;
+}
+int a1=78456445;
+Lock(a1);
+Console.WriteLine(a1);
