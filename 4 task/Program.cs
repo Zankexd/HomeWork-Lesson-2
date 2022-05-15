ebca07793ff4a7fe578878bbf569c011b85229c2
Oleksii <zankexd@icloud.com>
2022-05-15 18:05:30 +0200

    lesson

---
+++ 4 task/Program.cs
@@ -0,0 +1,15 @@
+﻿string a = Console.ReadLine();
+int a2 = int.Parse (a);
+if (a2==6)
+{
+    Console.Write("Суббота");
+
+}
+else if(a2==7)
+{
+Console.WriteLine ("Воскреснье");
+
+}
+else {
+    Console.Write("Пора работать, нет выходного");
+}
